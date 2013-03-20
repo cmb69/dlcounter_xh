de3dc68f9d5698ae142d701393aeda0946748a08
cmb69 <cmbecker69@gmx.de>
2013-03-20 21:46:46 +0000

    added Estonian localization (contributed by tanavots)

---
+++ languages/et.php
@@ -0,0 +1,20 @@
+<?php
+
+	$plugin_tx['dlcounter']['menu_main']="Statistika";
+	$plugin_tx['dlcounter']['syscheck_title']="Süsteemi kontroll";
+	$plugin_tx['dlcounter']['syscheck_phpversion']="PHP versioon ≥ %s";
+	$plugin_tx['dlcounter']['syscheck_extension']="Laiend '%s' laaditud";
+	$plugin_tx['dlcounter']['syscheck_encoding']="'UTF-8' kodeering seadistatud";
+	$plugin_tx['dlcounter']['syscheck_magic_quotes']="magic_quotes_runtime väljas";
+	$plugin_tx['dlcounter']['syscheck_jquery']="<a href=\"http://www.cmsimple-xh.com/wiki/doku.php/plugins:jquery4cmsimple\">jQuery4CMSimple lisa</a> paigaldatud";
+	$plugin_tx['dlcounter']['syscheck_writable']="Kaust '%s' on kirjutatav";
+	$plugin_tx['dlcounter']['label_totals']="Kogusumma";
+	$plugin_tx['dlcounter']['label_individual']="Üksikud allalaadimised";
+	$plugin_tx['dlcounter']['label_date']="Kuupäev";
+	$plugin_tx['dlcounter']['label_file']="Fail";
+	$plugin_tx['dlcounter']['label_count']="Arv";
+	$plugin_tx['dlcounter']['label_download']="Allalaadimised";
+	$plugin_tx['dlcounter']['cf_folder_data']="CMSimple juurkataloogile vastav kaust, kuhu lisade andmed salvestatakse, näit \"userfiles/dlcounter/\". Tühjaks jättes salvestatakse lisade data/ kausta.";
+	$plugin_tx['dlcounter']['cf_folder_downloads']="CMSimple juurkataloogile vastav kaust, kus allalaaditavad failid asuvad, näit \"userfiles/downloads/\".";
+
+?>
