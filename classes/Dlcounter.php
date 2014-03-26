3ad83ed63377007cafc2f003292a5c8050a4e0dd
cmb69 <cmbecker69@gmx.de>
2014-03-26 02:53:29 +0000

    refactored procedural code to OO

---
+++ classes/Dlcounter.php
@@ -0,0 +1,356 @@
+<?php
+
+/**
+ * Dlcounter_XH classes.
+ *
+ * PHP version 5
+ *
+ * @category  CMSimple_XH
+ * @package   Dlcounter
+ * @author    Christoph M. Becker <cmbecker69@gmx.de>
+ * @copyright 2012-2014 Christoph M. Becker <http://3-magi.net>
+ * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @version   SVN: $Id$
+ * @link      http://3-magi.net/?CMSimple_XH/Dlcounter_XH
+ */
+
+/**
+ * The general purpose class.
+ *
+ * @category CMSimple_XH
+ * @package  Dlcounter
+ * @author   Christoph M. Becker <cmbecker69@gmx.de>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @link     http://3-magi.net/?CMSimple_XH/Dlcounter_XH
+ */
+class Dlcounter
+{
+    /**
+     * Returns the data folder's path.
+     *
+     * @return string
+     *
+     * @global array The path of system files and folders.
+     * @global array The configuration of the plugins.
+     */
+    protected function dataFolder()
+    {
+        global $pth, $plugin_cf;
+
+        $pcf = $plugin_cf['dlcounter'];
+        if (empty($pcf['folder_data'])) {
+            $fn = $pth['folder']['plugins'] . 'dlcounter/data/';
+        } else {
+            $fn = $pth['folder']['base'] . $pcf['folder_data'];
+            if ($fn{strlen($fn) - 1} != '/') {
+                $fn .= '/';
+            }
+        }
+        if (file_exists($fn)) {
+            if (!is_dir($fn)) {
+                e('cntopen', 'folder', $fn);
+            }
+        } else {
+            if (!mkdir($fn, 0777)) {
+                e('cntsave', 'folder', $fn);
+            }
+        }
+        return $fn;
+    }
+
+    /**
+     * Returns whether a log entry for the download was appended.
+     *
+     * @param string $file A filename.
+     *
+     * @return bool
+     *
+     * @global bool Whether we're in admin mode.
+     */
+    protected function log($file)
+    {
+        global $adm;
+
+        $rec = $adm ? '' : time() . "\t" . basename($file) . "\n";
+        $fn = $this->dataFolder() . 'downloads.dat';
+        if (($fh = fopen($fn, 'a')) !== false && fwrite($fh, $rec) !== false) {
+            $ok = true;
+        } else {
+            $ok = false;
+            e('cntwriteto', 'file', $fn);
+        }
+        if ($fh !== false) {
+            fclose($fh);
+        }
+        return $ok;
+    }
+
+    /**
+     * Delivers the download.
+     *
+     * @param string $fn A filename.
+     *
+     * @return void
+     *
+     * @global array The paths of system files and folders.
+     * @global array The configuration of the plugins.
+     */
+    public function download($fn)
+    {
+        global $pth, $plugin_cf;
+
+        $pcf = $plugin_cf['dlcounter'];
+
+        $fn = $pth['folder']['base'] . $pcf['folder_downloads'] . basename($fn);
+        if (is_readable($fn)) {
+            if ($this->log($fn)) {
+                header('Content-Description: File Transfer');
+                header('Content-Type: application/octet-stream');
+                header('Content-Disposition: attachment; filename=' . basename($fn));
+                header('Content-Transfer-Encoding: binary');
+                header('Expires: 0');
+                header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+                header('Pragma: public');
+                header('Content-Length: ' . filesize($fn));
+                if (ob_get_level()) {
+                    ob_end_clean();
+                }
+                readfile($fn);
+                exit;
+            }
+        } else {
+            shead('404');
+        }
+    }
+
+    /**
+     * Returns the download form view.
+     *
+     * @param string $fn A filename.
+     *
+     * @return string (X)HTML.
+     *
+     * @global string The URL of the current page.
+     * @global array  The paths of system files and folders.
+     * @global array  The configuration of the plugins.
+     * @global array  The localization of the plugins.
+     */
+    public function main($fn)
+    {
+        global $su, $pth, $plugin_cf, $plugin_tx;
+
+        $units = array('B', 'KB', 'MB', 'GB');
+        $pcf = $plugin_cf['dlcounter'];
+
+        $ffn = $pth['folder']['base'] . $pcf['folder_downloads'] . $fn;
+        if (!is_readable($ffn)) {
+            e('notreadable', 'file', $ffn);
+            return false;
+        }
+        $size = filesize($ffn);
+        $log = intval(log($size, 1024));
+        $size = round($size / pow(1024, $log), 1) . ' ' . $units[$log];
+        return '<form class="dlcounter" action="?' . $su . '" method="GET">'
+            . tag('input type="hidden" name="dlcounter" value="' . $fn . '"')
+            . tag(
+                'input type="image" src="' . $pth['folder']['plugins']
+                . 'dlcounter/images/download-button.png"'
+                . ' alt="' . $plugin_tx['dlcounter']['label_download'] . '"'
+                . ' title="' . $fn . ' &ndash; ' . $size . '"'
+            )
+            . '</form>';
+    }
+
+    /**
+     * Returns the content of the downloads database.
+     *
+     * @return array
+     *
+     * @global array The paths of system files and folders.
+     */
+    protected function readDb()
+    {
+        global $pth;
+
+        $data = array();
+        $fn = $this->dataFolder() . 'downloads.dat';
+        $lines = file($fn);
+        if ($lines !== false) {
+            foreach ($lines as $line) {
+                $data[] = explode("\t", rtrim($line));
+            }
+        } else {
+            e('cntopen', 'file', $fn);
+        }
+        return $data;
+    }
+
+    /**
+     * Outputs the JS to initialize the tablesorter to <head>.
+     *
+     * @return void
+     *
+     * @global array  The paths of system files and folders.
+     * @global string The (X)HTML to insert into the head element.
+     */
+    protected function hjs()
+    {
+        global $pth, $hjs;
+
+        include_once $pth['folder']['plugins'] . 'jquery/jquery.inc.php';
+        include_jQuery();
+        include_jQueryPlugin(
+            'tablesorter',
+            $pth['folder']['plugins'] . 'dlcounter/lib/jquery.tablesorter.js'
+        );
+        $hjs .= <<<SCRIPT
+<script type="text/javascript">
+/* <![CDATA[ */
+jQuery(function() {
+    jQuery('table.tablesorter').tablesorter()
+})
+/* ]]> */
+</script>
+
+SCRIPT;
+    }
+
+    /**
+     * Returns the plugin information view.
+     *
+     * @return string (X)HTML.
+     *
+     * @global array The paths of system files and folders.
+     */
+    public function version()
+    {
+        global $pth;
+
+        return '<h1>Dlcounter_XH</h1>'
+            . tag(
+                'img class="dlcounter_plugin_icon" src="'
+                . $pth['folder']['plugins'] . 'dlcounter/dlcounter.png" width="128"'
+                . ' height="128" alt="Plugin Icon"'
+            )
+            . '<p>Version: ' . DLCOUNTER_VERSION . '</p>'
+            . '<p>Copyright &copy; 2012'
+            . ' <a href="http://3-magi.net">Christoph M. Becker</a></p>'
+            . '<p class="dlcounter_license">'
+            . 'This program is free software: you can redistribute it and/or modify'
+            . ' it under the terms of the GNU General Public License as published by'
+            . ' the Free Software Foundation, either version 3 of the License, or'
+            . ' (at your option) any later version.</p>'
+            . '<p class="dlcounter_license">'
+            . 'This program is distributed in the hope that it will be useful,'
+            . ' but WITHOUT ANY WARRANTY; without even the implied warranty of'
+            . ' MERCHAN&shy;TABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the'
+            . ' GNU General Public License for more details.</p>'
+            . '<p class="dlcounter_license">'
+            . 'You should have received a copy of the GNU General Public License'
+            . ' along with this program.  If not, see'
+            . ' <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/'
+            . '</a>.</p>';
+    }
+
+    /**
+     * Returns the requirements information view.
+     *
+     * @return string (X)HTML.
+     *
+     * @global array The paths of system files and folders.
+     * @global array The localization of the core.
+     * @global array The localization of the plugins.
+     * @global array The configuration of the plugins.
+     */
+    public function systemCheck()
+    {
+        global $pth, $tx, $plugin_tx, $plugin_cf;
+
+        define('DLCOUNTER_PHP_VERSION', '4.2.0');
+        $ptx = $plugin_tx['dlcounter'];
+        $imgdir = $pth['folder']['plugins'] . 'dlcounter/images/';
+        $ok = tag('img src="'.$imgdir.'ok.png" alt="ok"');
+        $warn = tag('img src="'.$imgdir.'warn.png" alt="warning"');
+        $fail = tag('img src="'.$imgdir.'fail.png" alt="failure"');
+        $o = '<h4>' . $ptx['syscheck_title'] . '</h4>'
+            . (version_compare(PHP_VERSION, DLCOUNTER_PHP_VERSION) >= 0
+                ? $ok : $fail)
+            . '&nbsp;&nbsp;'
+            . sprintf($ptx['syscheck_phpversion'], DLCOUNTER_PHP_VERSION)
+            . tag('br') . tag('br');
+        foreach (array('date') as $ext) {
+            $o .= (extension_loaded($ext) ? $ok : $fail)
+                . '&nbsp;&nbsp;' . sprintf($ptx['syscheck_extension'], $ext)
+                . tag('br');
+        }
+        $o .= tag('br')
+            . (strtoupper($tx['meta']['codepage']) == 'UTF-8' ? $ok : $warn)
+            . '&nbsp;&nbsp;' . $ptx['syscheck_encoding'] . tag('br')
+            . (!get_magic_quotes_runtime() ? $ok : $warn)
+            . '&nbsp;&nbsp;' . $ptx['syscheck_magic_quotes'] . tag('br')
+            . (file_exists($pth['folder']['plugins'] . 'jquery/jquery.inc.php')
+                ? $ok : $fail)
+            . '&nbsp;&nbsp;' . $ptx['syscheck_jquery'] . tag('br') . tag('br');
+        foreach (array('config/', 'css/', 'languages/') as $folder) {
+            $folders[] = $pth['folder']['plugins'] . 'dlcounter/' . $folder;
+        }
+        $folders[] = $this->dataFolder();
+        foreach ($folders as $folder) {
+            $o .= (is_writable($folder) ? $ok : $warn)
+                . '&nbsp;&nbsp;' . sprintf($ptx['syscheck_writable'], $folder)
+                . tag('br');
+        }
+        return $o;
+    }
+
+    /**
+     * Returns the statistics view.
+     *
+     * @return string (X)HTML.
+     *
+     * @global array The localization of the plugins.
+     */
+    public function adminMain()
+    {
+        global $plugin_tx;
+
+        $ptx = $plugin_tx['dlcounter'];
+        $this->hjs();
+        $data = $this->readDb();
+
+        $o = '<div id="dlcounter_stats">';
+
+        $o .= '<div class="plugineditcaption">Dlcounter</div>';
+
+        $totals = array_count_values(
+            array_map(create_function('$elt', 'return $elt[1];'), $data)
+        );
+        $o .= '<h4 onclick="jQuery(this).next().toggle()">' . $ptx['label_totals']
+            . '</h4>'
+            .'<table class="tablesorter"><thead><tr>'
+            .'<th>' . $ptx['label_file'] . '</th><th>' . $ptx['label_count']
+            . '</th>'
+            .'</tr></thead><tbody>';
+        foreach ($totals as $file => $count) {
+            $o .= '<tr><td>' . $file . '</td><td>' . $count . '</td></tr>';
+        }
+        $o .= '</tbody></table>';
+
+        $o .= '<h4 onclick="jQuery(this).next().toggle()">'
+            . $ptx['label_individual'] . '</h4>'
+            .'<table class="tablesorter"><thead><tr>'
+            .'<th>'. $ptx['label_date'] . '</th><th>'. $ptx['label_file'] . '</th>'
+            .'</tr></thead><tbody>';
+        foreach ($data as $rec) {
+            $o .= '<tr><td>' . date('Y-m-d H:i:s', $rec[0]) . '</td>'
+                . '<td>' . $rec[1] . '</td></tr>';
+        }
+        $o .= '</tbody></table>';
+
+        $o .= '</div>';
+
+        return $o;
+    }
+}
+
+?>
--- index.php
+++ index.php
@@ -23,107 +23,21 @@ if (!defined('CMSIMPLE_XH_VERSION')) {
 }
 
 /**
+ * The plugin classes.
+ */
+require_once $pth['folder']['plugin_classes'] . 'Dlcounter.php';
+
+/**
  * The plugin version.
  */
 define('DLCOUNTER_VERSION', '1alpha1');
 
 /**
- * Returns the data folder's path.
+ * The plugin object.
  *
- * @return string
- *
- * @global array The path of system files and folders.
- * @global array The configuration of the plugins.
+ * @var Dlcounter
  */
-function Dlcounter_dataFolder()
-{
-    global $pth, $plugin_cf;
-
-    $pcf = $plugin_cf['dlcounter'];
-    if (empty($pcf['folder_data'])) {
-        $fn = $pth['folder']['plugins'] . 'dlcounter/data/';
-    } else {
-        $fn = $pth['folder']['base'] . $pcf['folder_data'];
-        if ($fn{strlen($fn) - 1} != '/') {
-            $fn .= '/';
-        }
-    }
-    if (file_exists($fn)) {
-        if (!is_dir($fn)) {
-            e('cntopen', 'folder', $fn);
-        }
-    } else {
-        if (!mkdir($fn, 0777)) {
-            e('cntsave', 'folder', $fn);
-        }
-    }
-    return $fn;
-}
-
-/**
- * Returns whether a log entry for the download was appended.
- *
- * @param string $file A filename.
- *
- * @return bool
- *
- * @global bool Whether we're in admin mode.
- */
-function Dlcounter_log($file)
-{
-    global $adm;
-
-    $rec = $adm ? '' : time() . "\t" . basename($file) . "\n";
-    $fn = Dlcounter_dataFolder() . 'downloads.dat';
-    if (($fh = fopen($fn, 'a')) !== false && fwrite($fh, $rec) !== false) {
-        $ok = true;
-    } else {
-        $ok = false;
-        e('cntwriteto', 'file', $fn);
-    }
-    if ($fh !== false) {
-        fclose($fh);
-    }
-    return $ok;
-}
-
-/**
- * Delivers the download.
- *
- * @param string $fn A filename.
- *
- * @return void
- *
- * @global array The paths of system files and folders.
- * @global array The configuration of the plugins.
- */
-function Dlcounter_download($fn)
-{
-    global $pth, $plugin_cf;
-
-    $pcf = $plugin_cf['dlcounter'];
-
-    $fn = $pth['folder']['base'] . $pcf['folder_downloads'] . basename($fn);
-    if (is_readable($fn)) {
-        if (Dlcounter_log($fn)) {
-            header('Content-Description: File Transfer');
-            header('Content-Type: application/octet-stream');
-            header('Content-Disposition: attachment; filename=' . basename($fn));
-            header('Content-Transfer-Encoding: binary');
-            header('Expires: 0');
-            header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-            header('Pragma: public');
-            header('Content-Length: ' . filesize($fn));
-            if (ob_get_level()) {
-                ob_end_clean();
-            }
-            readfile($fn);
-            exit;
-        }
-    } else {
-        shead('404');
-    }
-}
+$_Dlcounter = new Dlcounter();
 
 /**
  * Returns the download form view.
@@ -131,43 +45,19 @@ function Dlcounter_download($fn)
  * @param string $fn A filename.
  *
  * @return string (X)HTML.
- *
- * @global string The URL of the current page.
- * @global array  The paths of system files and folders.
- * @global array  The configuration of the plugins.
- * @global array  The localization of the plugins.
  */
 function dlcounter($fn)
 {
-    global $su, $pth, $plugin_cf, $plugin_tx;
+    global $_Dlcounter;
 
-    $units = array('B', 'KB', 'MB', 'GB');
-    $pcf = $plugin_cf['dlcounter'];
-
-    $ffn = $pth['folder']['base'] . $pcf['folder_downloads'] . $fn;
-    if (!is_readable($ffn)) {
-        e('notreadable', 'file', $ffn);
-        return false;
-    }
-    $size = filesize($ffn);
-    $log = intval(log($size, 1024));
-    $size = round($size / pow(1024, $log), 1) . ' ' . $units[$log];
-    return '<form class="dlcounter" action="?' . $su . '" method="GET">'
-        . tag('input type="hidden" name="dlcounter" value="' . $fn . '"')
-        . tag(
-            'input type="image" src="' . $pth['folder']['plugins']
-            . 'dlcounter/images/download-button.png"'
-            . ' alt="' . $plugin_tx['dlcounter']['label_download'] . '"'
-            . ' title="' . $fn . ' &ndash; ' . $size . '"'
-        )
-        . '</form>';
+    return $_Dlcounter->main($fn);
 }
 
 /*
  * Handle the download request.
  */
 if (isset($_GET['dlcounter']) && $_GET['dlcounter'] !== '') {
-    Dlcounter_download(stsl($_GET['dlcounter']));
+    $_Dlcounter->download(stsl($_GET['dlcounter']));
 }
 
 ?>
