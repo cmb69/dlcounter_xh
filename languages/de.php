6508bae173d28cf6857131e2dfb917a749dd9494
cmb69 <cmbecker69@gmx.de>
2014-03-28 18:37:45 +0000

    added German localization

---
+++ languages/de.php
@@ -0,0 +1,26 @@
+<?php
+
+$plugin_tx['dlcounter']['menu_main']="Statistiken";
+
+$plugin_tx['dlcounter']['syscheck_title']="Systemprüfung";
+$plugin_tx['dlcounter']['syscheck_phpversion']="PHP Version &ge; %s";
+$plugin_tx['dlcounter']['syscheck_extension']="Erweiterung \"%s\" geladen";
+$plugin_tx['dlcounter']['syscheck_encoding']="Kodierung \"UTF-8\" eingestellt";
+$plugin_tx['dlcounter']['syscheck_magic_quotes']="magic_quotes_runtime Off";
+$plugin_tx['dlcounter']['syscheck_jquery']="<a href=\"http://www.cmsimple-xh.org/wiki/doku.php/extend:jquery4cmsimple\">jQuery4CMSimple plugin</a> installiert";
+$plugin_tx['dlcounter']['syscheck_writable']="Ordner \"%s\" beschreibbar";
+
+$plugin_tx['dlcounter']['label_totals']="Übersicht";
+$plugin_tx['dlcounter']['label_individual']="Einzelne Downloads";
+$plugin_tx['dlcounter']['label_date']="Datum";
+$plugin_tx['dlcounter']['label_file']="Datei";
+$plugin_tx['dlcounter']['label_count']="Anzahl";
+$plugin_tx['dlcounter']['label_download']="Download";
+
+$plugin_tx['dlcounter']['message_cantread']="Datei \"%s\" nicht lesbar!";
+$plugin_tx['dlcounter']['message_cantwrite']="Datei \"%s\" nicht schreibbar!";
+
+$plugin_tx['dlcounter']['cf_folder_data']="Der Pfad eines Ordner relativ zum Installationsverzeichnis von CMSimple_XH, wo die Daten des Plugins gespeichert werden sollen, z.B. \"userfiles/dlcounter/\". Ist diese Option leer, dann wird im data/ Order des Plugin gespeichert.";
+$plugin_tx['dlcounter']['cf_folder_downloads']="Der Pfad eines Ordners relativ zum Installationsverzeichnis von CMSimple_XH, wo die herunter zu ladenden Dateien gespeichert werden, z.B. \"userfiles/downloads/\".";
+
+?>
--- languages/en.php
+++ languages/en.php
@@ -7,7 +7,7 @@ $plugin_tx['dlcounter']['syscheck_phpversion']="PHP version &ge; %s";
 $plugin_tx['dlcounter']['syscheck_extension']="Extension \"%s\" loaded";
 $plugin_tx['dlcounter']['syscheck_encoding']="Encoding \"UTF-8\" configured";
 $plugin_tx['dlcounter']['syscheck_magic_quotes']="magic_quotes_runtime Off";
-$plugin_tx['dlcounter']['syscheck_jquery']="<a href=\"http://www.cmsimple-xh.com/wiki/doku.php/plugins:jquery4cmsimple\">jQuery4CMSimple plugin</a> installed";
+$plugin_tx['dlcounter']['syscheck_jquery']="<a href=\"http://www.cmsimple-xh.org/wiki/doku.php/extend:jquery4cmsimple\">jQuery4CMSimple plugin</a> installed";
 $plugin_tx['dlcounter']['syscheck_writable']="Folder \"%s\" writable";
 
 $plugin_tx['dlcounter']['label_totals']="Totals";
@@ -20,7 +20,7 @@ $plugin_tx['dlcounter']['label_download']="Download";
 $plugin_tx['dlcounter']['message_cantread']="Can't read file \"%s\"!";
 $plugin_tx['dlcounter']['message_cantwrite']="Can't write to file \"%s\"!";
 
-$plugin_tx['dlcounter']['cf_folder_data']="Path to a folder relative to the CMSimple root directory, where to store the plugin's data, e.g. \"userfiles/dlcounter/\". Leave empty to store in the plugin's data/ folder.";
-$plugin_tx['dlcounter']['cf_folder_downloads']="Path to a folder relative to the CMSimple root directory, where the downloadable files are stored, e.g. \"userfiles/downloads/\".";
+$plugin_tx['dlcounter']['cf_folder_data']="The path of a folder relative to the CMSimple_XH root directory, where to store the plugin's data, e.g. \"userfiles/dlcounter/\". Leave empty to store in the plugin's data/ folder.";
+$plugin_tx['dlcounter']['cf_folder_downloads']="The path of a folder relative to the CMSimple_XH root directory, where the downloadable files are stored, e.g. \"userfiles/downloads/\".";
 
 ?>
