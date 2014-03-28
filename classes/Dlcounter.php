9f476079dc76c288c04f13b73214680d07afa530
cmb69 <cmbecker69@gmx.de>
2014-03-28 14:23:55 +0000

    removed unnecessary download headers

--- classes/Dlcounter.php
+++ classes/Dlcounter.php
@@ -126,13 +126,8 @@ class Dlcounter
      */
     protected function deliverDownload($filename)
     {
-        header('Content-Description: File Transfer');
         header('Content-Type: application/octet-stream');
         header('Content-Disposition: attachment; filename=' . basename($filename));
-        header('Content-Transfer-Encoding: binary');
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-        header('Pragma: public');
         header('Content-Length: ' . filesize($filename));
         readfile($filename);
         Dlcounter_exit();
