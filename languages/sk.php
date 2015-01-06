501fe02bf68867eb53ec6a7822b78a1c54f49a6c
Christoph M. Becker <cmbecker69@gmx.de>
2015-01-07 00:21:09 +0100

    added Slovak localization (contributed by Tata)

---
+++ languages/sk.php
@@ -0,0 +1,25 @@
+<?php
+
+$plugin_tx['dlcounter']['menu_main']="Štatistika";
+
+$plugin_tx['dlcounter']['syscheck_title']="Kontrola systému";
+$plugin_tx['dlcounter']['syscheck_phpversion']="PHP verzia &ge; %s";
+$plugin_tx['dlcounter']['syscheck_extension']="Rozšírenie '%s' načítané";
+$plugin_tx['dlcounter']['syscheck_encoding']="Kódovanie 'UTF-8' nastavené";
+$plugin_tx['dlcounter']['syscheck_magic_quotes']="magic_quotes_runtime off";
+$plugin_tx['dlcounter']['syscheck_jquery']="<a href=\"http://www.cmsimple-xh.com/wiki/doku.php/plugins:jquery4cmsimple\">jQuery4CMSimple plugin</a> installed";
+$plugin_tx['dlcounter']['syscheck_writable']="Adresár '%s' umožňuje zápis";
+
+$plugin_tx['dlcounter']['label_totals']="Spolu";
+$plugin_tx['dlcounter']['label_individual']="Individuálne stiahnutia";
+$plugin_tx['dlcounter']['label_date']="Dátum";
+$plugin_tx['dlcounter']['label_file']="Súbor";
+$plugin_tx['dlcounter']['label_count']="Počet";
+$plugin_tx['dlcounter']['label_download']="Stiahnutie";
+$plugin_tx['dlcounter']['label_downloaded']="Stiahnuté";
+$plugin_tx['dlcounter']['label_downloaded_times']="krát";
+
+$plugin_tx['dlcounter']['cf_folder_data']="Cesta k adresáru relatívna ku koreňovému adresáru CMSimple, kam sa uložia dáta pluginu, napr. \"userfiles/dlcounter/\". Ak necháte prázdne, dáta sa uložia do adresára data/ pluginu.";
+$plugin_tx['dlcounter']['cf_folder_downloads']="Cesta k adresáru relatívna ku koreňovému adresáru CMSimple, kam sa uložia súbory určené na stiahnutie, napr. \"userfiles/downloads/\".";
+
+?>
