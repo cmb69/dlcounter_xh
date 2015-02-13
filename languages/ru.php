6612bd483c5e24e80d6cba5fbbc643777553fb23
Christoph M. Becker <cmbecker69@gmx.de>
2015-02-13 14:39:25 +0100

    added Russian localization (contributed by Old)

---
+++ languages/ru.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * Copyright (c) 2012 Old aka Lybomyr Kydray
+ *
+ * Translation are licensed under the GNU General Public License, version 2 or
+ * later.
+ */
+
+$plugin_tx['dlcounter']['menu_main']="Статистика";
+/* added*/
+$plugin_tx['dlcounter']['menu_css']="Стили";
+$plugin_tx['dlcounter']['menu_config']="Конфигурация";
+$plugin_tx['dlcounter']['menu_language']="Язык";
+$plugin_tx['dlcounter']['menu_help']="Помощь";
+/* /added*/
+$plugin_tx['dlcounter']['syscheck_title']="Проверка системы";
+$plugin_tx['dlcounter']['syscheck_phpversion']="Версия PHP &ge; %s";
+$plugin_tx['dlcounter']['syscheck_extension']="Расширение '%s' загружено";
+$plugin_tx['dlcounter']['syscheck_encoding']="Кодировка 'UTF-8' установлена";
+$plugin_tx['dlcounter']['syscheck_magic_quotes']="Экранирование спецсимволов (\"magic_quotes_runtime\") отключено";
+$plugin_tx['dlcounter']['syscheck_jquery']="Плагин <a href=\"http://www.cmsimple-xh.com/wiki/doku.php/plugins:jquery4cmsimple\">jQuery4CMSimple</a> установлен";
+$plugin_tx['dlcounter']['syscheck_writable']="Папка '%s' доступна для записи";
+$plugin_tx['dlcounter']['label_totals']="Всего";
+$plugin_tx['dlcounter']['label_individual']="Отдельные скачивания";
+$plugin_tx['dlcounter']['label_date']="Дата";
+$plugin_tx['dlcounter']['label_file']="Файл";
+$plugin_tx['dlcounter']['label_count']="Количество";
+$plugin_tx['dlcounter']['label_download']="Скачано";
+$plugin_tx['dlcounter']['cf_folder_data']="Путь к папке, где хранятся файлы данных, относительно корневого каталога CMSimple. Например, \"userfiles/dlcounter/\". Оставьте поле пустым для хранения данных в папке по умолчанию (каталог <b>data/</b> в директории плагина).";
+$plugin_tx['dlcounter']['cf_folder_downloads']="Путь к папке, где хранятся файлы для скачивания, относительно корневого каталога CMSimple. Например, \"userfiles/downloads/\".";
+
+?>
