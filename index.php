0d87a32bfdc0a2a8ae53b5badff157ab47b916a6
Christoph M. Becker <cmbecker69@gmx.de>
2017-05-03 16:34:00 +0200

    XH_exit() is available as of CMSimple_XH 1.6.2

--- index.php
+++ index.php
@@ -55,18 +55,3 @@ function Dlcounter_includeJQuery()
 
     include_once $pth['folder']['plugins'] . 'jquery/jquery.inc.php';
 }
-
-if (!function_exists('XH_exit')) {
-    /**
-     * Exits the script.
-     *
-     * We can't call exit directly, because that breaks unit tests,
-     * so we use this workaround.
-     *
-     * @return void
-     */
-    function XH_exit()
-    {
-        exit;
-    }
-}
