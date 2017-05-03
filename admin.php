d05e13d68be453987515fc0bfe9875d20e6ad478
Christoph M. Becker <cmbecker69@gmx.de>
2017-05-03 17:44:33 +0200

    Fix typo

--- admin.php
+++ admin.php
@@ -10,7 +10,7 @@
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Fa_XH is distributed in the hope that it will be useful,
+ * Dlcounter_XH is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
