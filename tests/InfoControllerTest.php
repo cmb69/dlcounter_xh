b24eafa80e9da042ec6a3987b2c582a5efad4f58
Christoph M. Becker <cmbecker69@gmx.de>
2025-03-13 16:06:18 +0100

    Test InfoController

---
+++ tests/InfoControllerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Dlcounter;
+
+use ApprovalTests\Approvals;
+use PHPUnit\Framework\TestCase;
+use Plib\FakeSystemChecker;
+use Plib\View;
+
+class InfoControllerTest extends TestCase
+{
+    public function testRendersInfo(): void
+    {
+        $sut = new InfoController(
+            "./",
+            new FakeSystemChecker(),
+            new View("./views/", XH_includeVar("./languages/en.php", "plugin_tx")["dlcounter"])
+        );
+        Approvals::verifyHtml($sut->defaultAction());
+    }
+}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,19 @@
 <?php
 
 require_once './vendor/autoload.php';
+
+require_once '../../cmsimple/functions.php';
+
 require_once '../plib/classes/SystemChecker.php';
 require_once '../plib/classes/View.php';
+require_once '../plib/classes/FakeSystemChecker.php';
+
 require_once './classes/DbService.php';
 require_once './classes/DownloadService.php';
 require_once './classes/Dic.php';
 require_once './classes/InfoController.php';
 require_once './classes/MainAdminController.php';
 require_once './classes/MainController.php';
+
+const CMSIMPLE_XH_VERSION = "CMSimple_XH 1.7.5";
+const DLCOUNTER_VERSION = "1.0beta2";
